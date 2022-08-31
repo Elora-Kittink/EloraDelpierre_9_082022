25c99d74b95795346a7dfc42810521c68cd28927
Elora <edelpierre@exomind.fr>
2022-08-31 10:12:18 +0200

    Rate data struct

---
+++ baluchon/Model/ExchangeRateService.swift
@@ -0,0 +1,8 @@
+//
+//  ExchangeRateService.swift
+//  baluchon
+//
+//  Created by Elora on 22/08/2022.
+//
+
+import Foundation
---
+++ baluchon/Model/ExchangeRateStruct.swift
@@ -0,0 +1,41 @@
+//
+//  ExchangeRateStruct.swift
+//  baluchon
+//
+//  Created by Elora on 25/08/2022.
+//
+
+import Foundation
+
+struct ExchangeRateStruct: Decodable {
+    struct Query: Decodable {
+        let from: String
+        let to: String
+        let amount: Double
+    }
+    struct Info: Decodable {
+        let timestamp: Int
+        let rate: Float
+    }
+    let info: Info?
+    let query: Query?
+    let success: Bool
+    let date: String
+    let result: Float?
+    
+}
+
+//{
+//    "success": true,
+//    "query": {
+//        "from": "EUR",
+//        "to": "GBP",
+//        "amount": 1
+//    },
+//    "info": {
+//        "timestamp": 1661417643,
+//        "rate": 0.84475
+//    },
+//    "date": "2022-08-25",
+//    "result": 0.84475
+//}
