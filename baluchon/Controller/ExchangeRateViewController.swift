dfca28e95ff3ced86df43da747c9333ac6870ef3
Elora <edelpierre@exomind.fr>
2022-08-31 10:15:39 +0200

    call API exchange rate OK

--- baluchon/Controller/ExchangeRateViewController.swift
+++ baluchon/Controller/ExchangeRateViewController.swift
@@ -7,22 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ExchangeViewController: UIViewController {
+    
+    @IBOutlet weak var euroLabel: UITextField! {
+        didSet {
+            self.euroLabel.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var dollarLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-    @IBAction private func didTapEuroExchangeField() {
-        
+    @IBAction private func didTapExchangeField(_ sender: UITextField) {
+        let from: String
+        let amount: Float
+        let to: String
+//        print(sender.text)
+        if sender == self.euroLabel {
+            from = "EUR"
+            let senderText = sender.text
+            amount = Float(senderText ?? "") ?? 0
+            to = "USD"
+        } else {
+            from = "USD"
+            let senderText = sender.text
+            amount = Float(senderText ?? "") ?? 0
+            to = "EUR"
+        }
+
+        ExchangeRate().getExchangeRate(amount: amount,
+                                       from: from,
+                                       to: to)
     }
     
-    @IBAction private func didTapDollarExchangeField() {
-        
+}
+
+extension ExchangeViewController: UITextFieldDelegate {
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        guard let oldText = textField.text, let r = Range(range, in: oldText) else { return false }
+        let newText = oldText.replacingCharacters(in: r, with: string)
+        guard let value = Double(newText.replacingOccurrences(of: ",", with: ".")) else { return false }
+        return true
     }
-    
-    
 
 }
 
+//^(100|[1-9][0-9]?(,[0-9][0-9]))$
--- baluchon/Model/ExchangeRate.swift
+++ baluchon/Model/ExchangeRate.swift
@@ -5,8 +5,30 @@
 //  Created by Elora on 10/08/2022.
 //
 
+import UIKit
 import Foundation
 
 class ExchangeRate {
     
+    var exchangeRateData: [ExchangeRateStruct] = []
+
+    let exchangeRateService = ExchangeRateService()
+    
+    func getExchangeRate(amount: Float, from: String, to: String) {
+        DispatchQueue.main.async {
+            self.exchangeRateService.fetchExchangeRate(amount: amount, from: from, to: to) { response in
+                if let rateData = response {
+                    print(rateData)
+                    self.exchangeRateData.append(rateData)
+                    print("TEST", self.exchangeRateData[0].result ?? 0)
+//                    print(self.exchangeRateData)
+                }
+            }
+        }
+//        print(exchangeRateData[0].result ?? "0")
+    }
+    
 }
+
+
+//  UTILISER LES CALLBACK 
