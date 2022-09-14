3c9b84e94d46f4b26111461c422f48e1faf58950
Elora <edelpierre@exomind.fr>
2022-09-14 16:07:15 +0200

    create reverse language function

--- baluchon/Model/WeatherService.swift
+++ baluchon/Model/WeatherService.swift
@@ -7,31 +7,54 @@
 
 import Foundation
 
-private let kApiKey = "6549c66045e304a584b58f52e94acf59"
+//private let kApiKey = "6549c66045e304a584b58f52e94acf59"
 
-class MeteoService {
+class WeatherService {
+    
+    static let shared = WeatherService()
+    private var weatherApiKey: String
+    
+        var weatherDataArray: [WeatherStruct] = []
     
     // MARK: - Variables
-    func fetchData(forCity: String, dataFetched: @escaping (MeteoResponse?) -> Void) {
-        let meteoUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?appid=\(kApiKey)&units=metric&q=\(forCity)")!
+    
+    let citys = ["Paris", "New York"]
+    
+    // MARK: - Init
+    private init() {
+        self.weatherApiKey = (Bundle.main.object(forInfoDictionaryKey: "Weather_API_KEY") as? String) ?? ""
+    }
+    
+    func fetchDataAwait(forCity: String) async -> WeatherStruct? {
+        let city = forCity.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+        let meteoUrl = URL(string: "https://api.openweathermap.org/data/2.5/weather?appid=\(self.weatherApiKey)&units=metric&q=\(city)")!
         
-        let task = URLSession.shared.dataTask(with: meteoUrl) { data, response, error in
-            guard
-                error == nil,
-                let data = data
-            else {
-                print(error ?? "Unknown error")
-                dataFetched(nil)
-                return
-            }
-            
-            let jsonDecoder = JSONDecoder()
-            let response = try? jsonDecoder.decode(MeteoResponse.self, from: data)
-            dataFetched(response)
+        do {
+            let (data, response) = try await URLSession.shared.data(from: meteoUrl)
+            let _data = try JSONDecoder().decode(WeatherStruct.self, from: data)
+            print(data)
+            return _data
+        } catch {
+            print(error)
+            return nil
         }
-        
-        task.resume()
+    }
+
+    
+    func weatherRequestAwait() async {
+        await withTaskGroup(of: Void.self) { group in
+            
+            self.citys.forEach { city in
+                group.addTask {
+                    print(city)
+                    let response = await self.fetchDataAwait(forCity: city)
+                    
+                    if let weatherData = response {
+                        self.weatherDataArray.append(weatherData)
+                    }
+                }
+            }
+            await group.waitForAll()
+        }
     }
 }
-
-// call API les génériques
