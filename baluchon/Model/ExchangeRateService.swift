a5e90220156e4ce1d0343fcb6e08f3ced0738350
Elora <edelpierre@exomind.fr>
2022-09-14 16:26:54 +0200

    changing @escape fetch for a async await fetch

--- baluchon/Model/ExchangeRateService.swift
+++ baluchon/Model/ExchangeRateService.swift
@@ -12,6 +12,24 @@ import Foundation
 class ExchangeRateService {
     
 
+    func fetchExchangeRate(amount: Float, from: String, to: String) async -> ExchangeRateStruct? {
+        
+        let apiKey = (Bundle.main.object(forInfoDictionaryKey: "Exchange_Rate_API_KEY") as? String) ?? ""
+        
+        let url2 = URL(string:"https://api.apilayer.com/fixer/convert?to=\(to)&from=\(from)&amount=\(amount)&apikey=\(apiKey)")!
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url2)
+            let _data = try JSONDecoder().decode(ExchangeRateStruct.self, from: data)
+            print(data)
+            return _data
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    
     
     func fetchExchangeRate(amount: Float, from: String, to: String, dataFetched: @escaping (ExchangeRateStruct?) -> Void) {
 
